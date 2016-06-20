a81369050379deb27a57537749c91f8e4d00549e
Julian Gruber <julian@juliangruber.com>
2016-06-20 18:42:21 +0200

    make hex check more solid

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ exports.encode = function (buf) {
 
 exports.decode = function (str) {
   str = str.slice(str.lastIndexOf('/') + 1)
-  if (str.length === 64) return Buffer.from(str, 'hex')
+  if (/[0-9a-f]{64}/i.test(str)) return Buffer.from(str, 'hex')
   if (str.length !== 50) throw new Error('Invalid key')
   return Buffer.from(encoding.decode(str))
 }
