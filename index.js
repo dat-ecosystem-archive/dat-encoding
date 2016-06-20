0c63760e5ca72677fc4ee858a3fd4ef16d076f0e
Julian Gruber <julian@juliangruber.com>
2016-06-20 11:19:02 +0200

    add verification of lengths

--- index.js
+++ index.js
@@ -2,11 +2,13 @@
 
 var baseX = require('base-x')
 var Buffer = require('safe-buffer').Buffer
+var assert = require('assert')
 
 var alphabet = '0123456789abcdefghijklmnopqrstuvwxyz'
 var encoding = baseX(alphabet)
 
 exports.encode = function (buf) {
+  assert(buf.length === 32, 'Invalid buffer')
   buf = Buffer.from(buf)
   return encoding.encode(buf)
 }
@@ -14,5 +16,6 @@ exports.encode = function (buf) {
 exports.decode = function (str) {
   str = str.slice(str.lastIndexOf('/') + 1)
   if (str.length === 64) return Buffer.from(str, 'hex')
+  assert(str.length === 50, 'Invalid key')
   return Buffer.from(encoding.decode(str))
 }
