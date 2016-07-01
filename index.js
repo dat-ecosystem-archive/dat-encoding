443b10946ed63d7bddb9b10dc7372eb7d5a0ef20
Julian Gruber <julian@juliangruber.com>
2016-07-01 20:06:27 +0200

    use - for padding

--- index.js
+++ index.js
@@ -9,12 +9,15 @@ var encoding = baseX(alphabet)
 exports.encode = function (buf) {
   if (buf.length !== 32) throw new Error('Invalid buffer')
   buf = Buffer.from(buf)
-  return encoding.encode(buf)
+  var str = encoding.encode(buf)
+  while (str.length < 50) str += '-'
+  return str
 }
 
 exports.decode = function (str) {
   str = str.slice(str.lastIndexOf('/') + 1)
   if (/[0-9a-f]{64}/i.test(str)) return Buffer.from(str, 'hex')
   if (str.length !== 50) throw new Error('Invalid key')
+  str = str.replace(/\-/g, '')
   return Buffer.from(encoding.decode(str))
 }
