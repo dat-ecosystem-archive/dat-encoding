ae3e35a4cdc5f2a46f857ad022b363e9cdbe0b23
Julian Gruber <julian@juliangruber.com>
2016-12-16 13:50:15 +0100

    add convenience methods .to{Buf,Str}, add id

--- README.md
+++ README.md
@@ -18,13 +18,19 @@ console.log('%s -> %s', buf, encoding.encode(buf))
 
 ## API
 
-### .encode(buf[, opts])
+### .encode(buf)
+### .toStr(buf)
 
 Encode `buf` into a hex string. Throws if `buf` isn't 32 bytes of length.
 
+If `buf` is already a string, checks if it's valid and returns it.
+
 ### .decode(str)
+### .toBuf(str)
 
 Decode `str` into its binary representation. Also supports `dat://` and `dat.com/` links. Throws if the raw link isn't 64 bytes of base64.
+
+If `str` is already a buffer, checks if it's valid and returns it.
 
 ## License
 
--- index.js
+++ index.js
@@ -2,13 +2,18 @@
 
 var Buffer = require('safe-buffer').Buffer
 
-exports.encode = function (buf, opts) {
+function encode (buf) {
+  if (typeof buf === 'string') return encode(decode(buf))
   if (buf.length !== 32) throw new Error('Invalid buffer')
   return Buffer.from(buf).toString('hex')
 }
 
-exports.decode = function (str) {
+function decode (str) {
+  if (Buffer.isBuffer(str)) return decode(encode(str))
   var match = /(?:[a-z]+:\/\/(?:dat\.land\/)?)?([^/]{64})/.exec(str)
   if (!match) throw new Error('Invalid key')
   return Buffer.from(match[1], 'hex')
 }
+
+exports.encode = exports.toBuf = encode
+exports.decode = exports.toStr = decode
