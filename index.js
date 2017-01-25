7a9fb585257a2771d08bdb3211f0e5024c059440
Julian Gruber <julian@juliangruber.com>
2017-01-25 11:23:26 +0100

    simplify regex. closes #18

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ function encode (buf) {
 
 function decode (str) {
   if (Buffer.isBuffer(str)) return decode(encode(str))
-  var match = /(?:[a-z]+:\/\/(?:dat\.land\/)?)?([^/]{64})/.exec(str)
+  var match = /\/?([^/]{64})/.exec(str)
   if (!match) throw new Error('Invalid key')
   return Buffer.from(match[1], 'hex')
 }
