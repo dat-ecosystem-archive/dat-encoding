17f27e8025c852c9de19b2fd9736b796e60cea32
Julian Gruber <julian@juliangruber.com>
2017-02-08 08:48:31 +0100

    improve error messages

--- index.js
+++ index.js
@@ -4,12 +4,14 @@ var Buffer = require('safe-buffer').Buffer
 
 function encode (buf) {
   if (typeof buf === 'string') return encode(decode(buf))
+  if (!Buffer.isBuffer(buf)) throw new Error('Not a buffer')
   if (buf.length !== 32) throw new Error('Invalid buffer')
-  return Buffer.from(buf).toString('hex')
+  return buf.toString('hex')
 }
 
 function decode (str) {
   if (Buffer.isBuffer(str)) return decode(encode(str))
+  if (typeof str !== 'string') throw new Error('Not a string')
   var match = /\/?([^/]{64})/.exec(str)
   if (!match) throw new Error('Invalid key')
   return Buffer.from(match[1], 'hex')
